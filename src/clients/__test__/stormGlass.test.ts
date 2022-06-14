0dfa13db9bb3b77734f688b844cb8c6394c39463
Icaro Peretti <icaroperetti50@gmail.com>
2022-06-13 22:59:25 -0300

    feat: Normalizing data response

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
+import stormGlassWeatherNormalized3hoursFixture from '../../../test/fixtures/stormglass_normalized_response_3_hours.json'
 import stormGlassWeather3HoursFixture from '../../../test/fixtures/stormglass_weather_3_hours.json'
-import stormGlassWeatherNormalized3hoursFixture from '../../../test/fixtures/stormglass_weather_normalized_3_hours.json'
 import { StormGlass } from '../stormGlass'
 jest.mock('axios')
 
@@ -9,7 +9,7 @@ describe('StormGlass client', () => {
     const lat = -33.792726
     const lng = 151.289824
 
-    axios.get = jest.fn().mockResolvedValue(stormGlassWeather3HoursFixture)
+    axios.get = jest.fn().mockResolvedValue({ data: stormGlassWeather3HoursFixture })
 
     const stormGlass = new StormGlass(axios)
     const response = await stormGlass.fetchPoints(lat, lng)
