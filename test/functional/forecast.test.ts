6e5477fd8da2b1d16eaa9645a3e80ed611890ddf
icaroperetti <icaroperetti50@gmail.com>
2022-07-30 13:25:36 -0300

    Adding nock

--- test/functional/forecast.test.ts
+++ test/functional/forecast.test.ts
@@ -1,6 +1,8 @@
 import { Beach, BeachPosition } from '@src/models/beach'
+import apiForecastResponse1BeachFixture from '@test/fixtures/api_forecast_response_1_beach.json'
 import stormGlassWeather3HoursFixture from '@test/fixtures/stormglass_weather_3_hours.json'
 import nock from 'nock'
+
 describe('Beach forecast functional tests', () => {
   beforeEach(async () => {
     await Beach.deleteMany({})
@@ -30,9 +32,26 @@ describe('Beach forecast functional tests', () => {
       })
       .reply(200, stormGlassWeather3HoursFixture)
 
-    // const { body, status } = await global.testRequest.get('/forecast')
-    // expect(status).toBe(200)
-    // // Make sure we use toEqual to check value not the object and array itself
-    // expect(body).toEqual(apiForecastResponse1BeachFixture)
+    const { body, status } = await global.testRequest.get('/forecast')
+    expect(status).toBe(200)
+    // Make sure we use toEqual to check value not the object and array itself
+    expect(body).toEqual(apiForecastResponse1BeachFixture)
+  })
+
+  it('should return 500 if something goes wrong during the processing', async () => {
+    nock('https://api.stormglass.io:443', {
+      encodedQueryParams: true,
+      reqheaders: {
+        Authorization: (): boolean => true,
+      },
+    })
+      .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
+      .get('/v1/weather/point')
+      .query({ lat: '-33.792726', lng: '151.289824' })
+      .replyWithError('Something went wrong')
+
+    const { status } = await global.testRequest.get(`/forecast`)
+
+    expect(status).toBe(500)
   })
 })
